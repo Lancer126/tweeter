a72cab3c9a924a3d55bd24d7c2f5edcead5beda0
Lancer126 <rthcontact@gmail.com>
2019-06-11 21:42:27 +0000

    Shows the count correctly

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
   var numLeft = 140;
     $(".new-tweet form textarea").on("input", function() {
         var text = $.trim($("textarea").val());
-        $(".new-tweet form conter")
-        console.log((140-text.length));
+        
+        $(".new-tweet form .counter").text(140-text.length);
+
+        if(text.length > 140) {
+          $(".new-tweet form .counter").css('color', 'red');
+        } else {
+          $(".new-tweet form .counter").css('color', 'black');
+        }
     });
   });
